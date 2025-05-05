3732587833796738c4e7b4d601f56c37b2dcb793
RaedRNP <raedrnp10@gmail.com>
2025-05-04 23:01:23 -0400

    Add image URL utility function

---
+++ src/utils/common/getImg.ts
@@ -0,0 +1,9 @@
+const BASE_URL = await import.meta.env.STRAPI_URL;
+
+export default function getProductoImg(url: string | null | undefined) {
+  if (url == null) return "/no-image.png";
+  if (url.startsWith("data:")) return url;
+  if (url.startsWith("http") || url.startsWith("//")) return url;
+
+  return `${BASE_URL}${url}`;
+}
