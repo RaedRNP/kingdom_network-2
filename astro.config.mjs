bef850130b99bcfac87672285763dd1e8ee783b2
RaedRNP <raedrnp10@gmail.com>
2025-05-09 02:00:30 -0400

    Add Svelte integration and server output
    Configure Astro for server output and add the Svelte
    integration along with necessary dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 import node from "@astrojs/node";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   adapter: node({
     mode: "standalone",
   }),
+
+  integrations: [svelte()],
 });
