05d1b7ae00d259248a02432fc4d4f2e3ef586b96
RaedRNP <raedrnp10@gmail.com>
2025-05-20 22:57:44 -0400

    Build site as static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,21 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
 
   vite: {
     plugins: [tailwindcss()],
   },
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 
   integrations: [svelte()],
 });
